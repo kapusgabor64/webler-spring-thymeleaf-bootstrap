765d06f225a0212d82f78e1b4d5cc99795c21413
Gabor <kapusgabor64@gmail.com>
2024-02-29 11:30:20 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerspringthymeleafbootstrap/WeblerSpringThymeleafBootstrapApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerspringthymeleafbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerSpringThymeleafBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerSpringThymeleafBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/webler/weblerspringthymeleafbootstrap/WeblerSpringThymeleafBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerspringthymeleafbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerSpringThymeleafBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
